ff5db526e4faf4a8cf2758c4c9c503725ce2c02b
yubincho <yubincho9@gmail.com>
2026-02-20 13:54:13 +0000

    행정동 코드 수정

--- portfolio 1/infra/airflow/dags/seoul/pipeline__seoul__end_to_end.py
+++ portfolio 1/infra/airflow/dags/seoul/pipeline__seoul__end_to_end.py
@@ -25,6 +25,9 @@ with DAG(
         poke_interval=30,
     )
 
+    # -----------------------------
+    # Living Population
+    # -----------------------------
     lp_2023 = TriggerDagRunOperator(
         task_id="ingest__livingpop__2023",
         trigger_dag_id="ingest__livingpop__year",
@@ -47,6 +50,9 @@ with DAG(
         poke_interval=30,
     )
 
+    # -----------------------------
+    # Sales
+    # -----------------------------
     sales = TriggerDagRunOperator(
         task_id="ingest__seoul_sales",
         trigger_dag_id="ingest__seoul_sales",
@@ -54,9 +60,22 @@ with DAG(
         poke_interval=30,
     )
 
+    # -----------------------------
+    # Vacancy
+    # -----------------------------
     vacancy = TriggerDagRunOperator(
         task_id="ingest__vacancy",
         trigger_dag_id="ingest__vacancy",
+        wait_for_completion=True,
+        poke_interval=30,
+    )
+
+    # -----------------------------
+    # Admin Dong 
+    # -----------------------------
+    prepare_admin = TriggerDagRunOperator(
+        task_id="prepare__admin_dong_source",
+        trigger_dag_id="prepare__admin_dong_source",
         wait_for_completion=True,
         poke_interval=30,
     )
@@ -70,4 +89,15 @@ with DAG(
 
     end = EmptyOperator(task_id="end")
 
-    start >> extract_all >> lp_2023 >> lp_2024 >> lp_2025 >> sales >> vacancy >> dim_admin >> end
+    (
+        start
+        >> extract_all
+        >> lp_2023
+        >> lp_2024
+        >> lp_2025
+        >> sales
+        >> vacancy
+        >> prepare_admin     # 먼저 csv 생성
+        >> dim_admin         # 그 다음 raw+dim 생성
+        >> end
+    )
